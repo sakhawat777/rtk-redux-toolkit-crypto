268815c93ffa920bb6bff7bf6e54f23820f59925
Md. Sakhawat Hossain <biplobbms@gmail.com>
2022-12-20 17:13:10 +0600

    Data Fetching Successful

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { useGetCoinsQuery } from './app/services/cryptoApi';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { data, isLoading, error } = useGetCoinsQuery();
+	console.log(data);
+	return (
+		<div>
+			<h1>Hello</h1>
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/provider';
+import { theme } from '@chakra-ui/theme';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<Provider store={store}>
+		<React.StrictMode>
+			<ChakraProvider theme={theme}>
+				<App />
+			</ChakraProvider>
+		</React.StrictMode>
+	</Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
